d971a968f1677e46c82a55ace5a0e5a0e87e4e57
908472814 <908472814@qq.com>
2018-10-18 21:53:43 +0800

    添加TODO

---
+++ entity_util/src/main/java/org/hhp/opensource/entityutil/EntityColumn.java
@@ -0,0 +1,41 @@
+package org.hhp.opensource.entityutil;
+
+public class EntityColumn {
+	private String name;
+	private String type;
+	private String rangeOfValue;
+	private ReferenceColnum referenceColnum;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getRangeOfValue() {
+		return rangeOfValue;
+	}
+
+	public void setRangeOfValue(String rangeOfValue) {
+		this.rangeOfValue = rangeOfValue;
+	}
+
+	public ReferenceColnum getReferenceColnum() {
+		return referenceColnum;
+	}
+
+	public void setReferenceColnum(ReferenceColnum referenceColnum) {
+		this.referenceColnum = referenceColnum;
+	}
+
+}
