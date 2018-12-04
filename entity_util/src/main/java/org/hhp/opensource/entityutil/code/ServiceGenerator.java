0c55927b77fa124acead161687f5ae259e4401c5
hehuabing <huabing.he@igen-tech.com>
2018-12-04 17:34:34 +0800

    增加代码模板

--- entity_util/src/main/java/org/hhp/opensource/entityutil/code/ServiceGenerator.java
+++ entity_util/src/main/java/org/hhp/opensource/entityutil/code/ServiceGenerator.java
@@ -1,5 +1,17 @@
 package org.hhp.opensource.entityutil.code;
 
-public class ServiceGenerator {
+import java.util.List;
 
+import org.hhp.opensource.entityutil.structure.TableEntity;
+import org.hhp.opensource.entityutil.structure.TableEntityColumn;
+import org.hhp.opensource.entityutil.util.Utils;
+
+public class ServiceGenerator {
+	
+	public void generate(List<TableEntity> entityList, String packageName, String target) {
+		entityList.forEach(entity -> {
+			String interfaceName = Utils.createClassName(entity.getEntityName()) + "Service";
+			TableEntityColumn pKey = entity.getPrimaryKey().get(0);
+		});
+	}
 }
